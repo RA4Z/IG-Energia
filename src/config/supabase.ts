591d065b624ccdd9a98df7597badc49c4c7fd91a
RA4Z <rraz639@gmail.com>
2024-05-06 14:16:26 -0300

    Alterando caminho do banco de dados default para evitar buscar de projeto criado para clientes

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://eljilalbzabmkahtvfkv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsamlsYWxiemFibWthaHR2Zmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1MjE1NDIsImV4cCI6MjAyODA5NzU0Mn0.xTIp3ClXqMbxFW98iHq_AxIzpz1Eq0YqiCyCPi_rZZQ'
+const supabaseUrl = 'https://idqwkijmiyvsuzxfzsyr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkcXdraWptaXl2c3V6eGZ6c3lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzMjAwNDcsImV4cCI6MjAyNzg5NjA0N30.l6V-KbiZ_gb6JDJajnLldvtfSGUOPsZZ9YmfYrbxNMY'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export { supabase }
